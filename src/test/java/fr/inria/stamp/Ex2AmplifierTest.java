245ade53577f658cc9373a6998d9b5ce533aff38
danglotb <bdanglot@gmail.com>
2017-11-28 16:15:23 +0100

    test: the amplification using Ex2Amplifier produces new test cases

--- src/test/java/fr/inria/stamp/Ex2AmplifierTest.java
+++ src/test/java/fr/inria/stamp/Ex2AmplifierTest.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
@@ -31,10 +32,83 @@ public class Ex2AmplifierTest extends AbstractTest {
         final CtClass<Object> testClass = factory.Class().get("fr.inria.calculator.CalculatorTest");
         amplifier.reset(testClass);
         final List<CtMethod> amplifiedTestAccumulate = amplifier.apply(testClass.getMethodsByName("testAccumulate").get(0));
-        System.out.println(amplifiedTestAccumulate);
-        final List<CtMethod> amplifiedTestAccumulateAndReset = amplifier.apply(testClass.getMethodsByName("testAccumulateAndReset").get(0));
-        System.out.println(amplifiedTestAccumulateAndReset);
+        assertEquals(expectedAmplifiedTestAccumulate, amplifiedTestAccumulate.toString());
     }
+
+    private static final String expectedAmplifiedTestAccumulate = "[@Test" + nl +
+            "public void testAccumulate_Ex2_0() throws Exception {" + nl +
+            "    final Calculator calculator1 = new Calculator(3);" + nl +
+            "    Assert.assertEquals((-5), calculator1.getCurrentValue());" + nl +
+            "    calculator1.accumulate(7);" + nl +
+            "    Assert.assertEquals((-15), calculator1.getCurrentValue());" + nl +
+            "}]";
+
+    @Test
+    public void testAmplifyTestDoubleAccumulate() throws Exception {
+        final String pathToConfiguration = "src/test/resources/calculator/calculator.properties";
+        final Ex2Amplifier amplifier = new Ex2Amplifier(pathToConfiguration);
+        final Factory factory = initSpoonModel(pathToConfiguration);
+        final CtClass<Object> testClass = factory.Class().get("fr.inria.calculator.CalculatorTest");
+        amplifier.reset(testClass);
+        final List<CtMethod> testDoubleAccumulate = amplifier.apply(testClass.getMethodsByName("testDoubleAccumulate").get(0));
+        assertEquals(2, testDoubleAccumulate.size());
+        assertEquals(expectedAmplifiedTestDoubleAccumulate, testDoubleAccumulate.toString());
+    }
+
+    private static final String expectedAmplifiedTestDoubleAccumulate = "[@Test" + nl +
+            "public void testDoubleAccumulate_Ex2_0() throws Exception {" + nl +
+            "    final Calculator calculator1 = new Calculator(3);" + nl +
+            "    Assert.assertEquals((-5), calculator1.getCurrentValue());" + nl +
+            "    calculator1.accumulate(7);" + nl +
+            "    Assert.assertEquals((-15), calculator1.getCurrentValue());" + nl +
+            "    calculator1.accumulate(-5);" + nl +
+            "    Assert.assertEquals(685, calculator1.getCurrentValue());" + nl +
+            "}, @Test" + nl +
+            "public void testDoubleAccumulate_Ex2_1() throws Exception {" + nl +
+            "    final Calculator calculator1 = new Calculator(-7);" + nl +
+            "    Assert.assertEquals((-5), calculator1.getCurrentValue());" + nl +
+            "    calculator1.accumulate(7);" + nl +
+            "    Assert.assertEquals((-15), calculator1.getCurrentValue());" + nl +
+            "    calculator1.accumulate(7);" + nl +
+            "    Assert.assertEquals(685, calculator1.getCurrentValue());" + nl +
+            "}]";
+
+    @Test
+    public void testAmplifyTestAccumulateAndReset() throws Exception {
+        final String pathToConfiguration = "src/test/resources/calculator/calculator.properties";
+        final Ex2Amplifier amplifier = new Ex2Amplifier(pathToConfiguration);
+        final Factory factory = initSpoonModel(pathToConfiguration);
+        final CtClass<Object> testClass = factory.Class().get("fr.inria.calculator.CalculatorTest");
+        amplifier.reset(testClass);
+        final List<CtMethod> testDoubleAccumulate = amplifier.apply(testClass.getMethodsByName("testAccumulateAndReset").get(0));
+        assertEquals(expectedAmplifiedTestAccumulateAndReset, testDoubleAccumulate.toString());
+    }
+
+    private static final String expectedAmplifiedTestAccumulateAndReset = "[@Test" + nl +
+            "public void testAccumulateAndReset_Ex2_0() throws Exception {" + nl +
+            "    final Calculator calculator1 = new Calculator(-5);" + nl +
+            "    Assert.assertEquals(3, calculator1.getCurrentValue());" + nl +
+            "    calculator1.accumulate(6);" + nl +
+            "    Assert.assertEquals(0, calculator1.getCurrentValue());" + nl +
+            "    calculator1.reset();" + nl +
+            "    Assert.assertEquals(0, calculator1.getCurrentValue());" + nl +
+            "}, @Test" + nl +
+            "public void testAccumulateAndReset_Ex2_1() throws Exception {" + nl +
+            "    final Calculator calculator1 = new Calculator(3);" + nl +
+            "    Assert.assertEquals(3, calculator1.getCurrentValue());" + nl +
+            "    calculator1.accumulate(4);" + nl +
+            "    Assert.assertEquals(0, calculator1.getCurrentValue());" + nl +
+            "    calculator1.reset();" + nl +
+            "    Assert.assertEquals(0, calculator1.getCurrentValue());" + nl +
+            "}, @Test" + nl +
+            "public void testAccumulateAndReset_Ex2_2() throws Exception {" + nl +
+            "    final Calculator calculator1 = new Calculator(3);" + nl +
+            "    Assert.assertEquals(3, calculator1.getCurrentValue());" + nl +
+            "    calculator1.accumulate(-8);" + nl +
+            "    Assert.assertEquals(0, calculator1.getCurrentValue());" + nl +
+            "    calculator1.reset();" + nl +
+            "    Assert.assertEquals(0, calculator1.getCurrentValue());" + nl +
+            "}]";
 
     private Factory initSpoonModel(String pathToConfigurationFile) {
         try {
