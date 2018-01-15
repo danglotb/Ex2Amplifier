76a285e19cb397d87a854711b702d49d91312921
danglotb <bdanglot@gmail.com>
2018-01-15 15:10:32 +0100

    install: will get and build z3

--- src/bash/install-dspot.sh
+++ src/bash/install-dspot.sh
@@ -9,3 +9,14 @@ cd ..
 git clone http://github.com/danglotb/jbse -b pre-java8
 cd jbse
 mvn install -DskipTests
+
+cd ..
+
+mkdir lib
+cd lib
+cd z3
+git clone https://github.com/Z3Prover/z3.git
+python scripts/mk_make.py --prefix=./
+cd build
+make
+sudo make install
