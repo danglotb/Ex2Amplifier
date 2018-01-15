19db31c5afcbf08b61dbf268c06a93d7e0897a78
danglotb <bdanglot@gmail.com>
2018-01-15 12:18:45 +0100

    feat: add Ex2Amplifier

---
+++ src/main/java/fr/inria/stamp/Ex2Amplifier.java
@@ -0,0 +1,110 @@
+package fr.inria.stamp;
+
+import fr.inria.diversify.automaticbuilder.AutomaticBuilderFactory;
+import fr.inria.diversify.dspot.amplifier.Amplifier;
+import fr.inria.diversify.dspot.support.DSpotCompiler;
+import fr.inria.diversify.utils.AmplificationHelper;
+import fr.inria.diversify.utils.DSpotUtils;
+import fr.inria.diversify.utils.sosiefier.InputConfiguration;
+import fr.inria.stamp.jbse.JBSERunner;
+import fr.inria.stamp.smt.SMTSolver;
+import spoon.reflect.code.CtLiteral;
+import spoon.reflect.code.CtUnaryOperator;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtParameter;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 09/01/18
+ */
+public class Ex2Amplifier implements Amplifier {
+
+    private InputConfiguration configuration;
+
+    public Ex2Amplifier(InputConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public List<CtMethod> apply(CtMethod ctMethod) {
+        if (!canBeRun.test(ctMethod)) {
+            return Collections.emptyList();
+        }
+        final CtMethod<?> extractedMethod = ArgumentsExtractor.performExtraction(ctMethod);
+        final CtType ctType = ctMethod.getParent(CtClass.class);
+        CtType<?> clone = ctType.clone();
+        clone.setParent(ctType.getParent());
+        clone.removeMethod(ctMethod);
+        clone.addMethod(extractedMethod);
+        DSpotUtils.printJavaFileWithComment(clone, new File(DSpotCompiler.pathToTmpTestSources));
+        final String classpath = AutomaticBuilderFactory
+                .getAutomaticBuilder(this.configuration)
+                .buildClasspath(this.configuration.getInputProgram().getProgramDir())
+                + AmplificationHelper.PATH_SEPARATOR +
+                this.configuration.getInputProgram().getProgramDir() + "/" + this.configuration.getInputProgram().getClassesDir()
+                + AmplificationHelper.PATH_SEPARATOR + "target/dspot/dependencies/"
+                + AmplificationHelper.PATH_SEPARATOR +
+                this.configuration.getInputProgram().getProgramDir() + "/" + this.configuration.getInputProgram().getTestClassesDir();
+        DSpotCompiler.compile(DSpotCompiler.pathToTmpTestSources, classpath,
+                new File(this.configuration.getInputProgram().getProgramDir() + "/" + this.configuration.getInputProgram().getTestClassesDir()));
+        final List<Map<String, List<String>>> conditionForEachParameterForEachState = JBSERunner.runJBSE(classpath, extractedMethod);
+        return conditionForEachParameterForEachState.stream()
+                .map(conditions ->
+                        this.generateNewTestMethod(ctMethod, conditions)
+                )
+                .collect(Collectors.toList());
+    }
+
+    private CtMethod<?> generateNewTestMethod(CtMethod<?> testMethod,
+                                              Map<String, List<String>> conditionForParameter) {
+        final CtMethod clone = AmplificationHelper.cloneMethodTest(testMethod, "_examplifier");
+        final List<?> solutions = SMTSolver.solve(conditionForParameter);
+        final Iterator<?> iterator = solutions.iterator();
+        final List<CtLiteral> originalLiterals =
+                clone.getElements(new TypeFilter<>(CtLiteral.class));
+        conditionForParameter.keySet()
+                .forEach(s -> {
+                    final int indexOfLit = Integer.parseInt(s.substring("param".length()));
+                    final CtLiteral literalToBeReplaced = originalLiterals.get(indexOfLit);
+                    final CtLiteral<?> newLiteral = testMethod.getFactory().createLiteral(iterator.next());
+                    if (literalToBeReplaced.getParent() instanceof CtUnaryOperator) {
+                        literalToBeReplaced.getParent().replace(newLiteral);
+                    } else {
+                        literalToBeReplaced.replace(newLiteral);
+                    }
+                });
+        return clone;
+    }
+
+    private final Predicate<CtMethod<?>> canBeRun = ctMethod ->
+            ctMethod.getParameters()
+                    .stream()
+                    .map(CtParameter::getType)
+                    .map(CtTypeReference::getSimpleName)
+                    .allMatch(JBSERunner.typeToDescriptor::containsKey
+                            // || "String".equals(typeStr)  TODO for now, we do not support String, because JBSE might take long time
+                    );
+
+    @Override
+    public CtMethod applyRandom(CtMethod ctMethod) {
+        return null;
+    }
+
+    @Override
+    public void reset(CtType ctType) {
+
+    }
+}
