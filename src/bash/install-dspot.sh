584f17d027a256aed7dc0a8b2ea9ab81ce0474db
danglotb <bdanglot@gmail.com>
2018-01-15 12:19:43 +0100

    script: update script to install JBSE

--- src/bash/install-dspot.sh
+++ src/bash/install-dspot.sh
@@ -3,3 +3,9 @@
 git clone http://github.com/STAMP-project/dspot.git
 cd dspot
 mvn install -DskipTests
+
+cd ..
+
+git clone http://github.com/danglotb/jbse -b pre-java8
+cd jbse
+mvn install -DskipTests
