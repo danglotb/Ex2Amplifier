8a3a3681f85d501f679a24228e64b7321fd5d1a6
danglotb <bdanglot@gmail.com>
2018-01-23 13:45:44 +0100

    feat: add MainGenerator

---
+++ src/main/java/fr/inria/stamp/catg/CATGUtils.java
@@ -0,0 +1,9 @@
+package fr.inria.stamp.catg;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 23/01/18
+ */
+public class CATGUtils {
+}
---
+++ src/main/java/fr/inria/stamp/catg/MainGenerator.java
@@ -0,0 +1,75 @@
+package fr.inria.stamp.catg;
+
+import com.sun.javafx.fxml.expression.Expression;
+import fr.inria.diversify.dspot.assertGenerator.AssertionRemover;
+import org.pitest.functional.FArray;
+import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtLiteral;
+import spoon.reflect.code.CtLocalVariable;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtParameter;
+import spoon.reflect.declaration.ModifierKind;
+import spoon.reflect.factory.Factory;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Benjamin DANGLOT
+ * benjamin.danglot@inria.fr
+ * on 23/01/18
+ */
+public class MainGenerator {
+
+    public static CtMethod<?> generateMainMethodFromTestMethod(CtMethod<?> testMethod) {
+        final Factory factory = testMethod.getFactory();
+        final CtBlock<?> blockMain =
+                new AssertionRemover().removeAssertion(testMethod).getBody().clone();
+        final List<CtLiteral<?>> originalLiterals =
+                blockMain.getElements(new TypeFilter<CtLiteral<?>>(CtLiteral.class));
+        final int[] count = new int[]{1};
+        final List<CtLocalVariable<?>> localVariables =
+                originalLiterals.stream()
+                        .map(literal ->
+                                factory.createLocalVariable(
+                                        literal.getType(),
+                                        "lit" + count[0]++,
+                                        factory.createCodeSnippetExpression(createMakeRead(factory, literal))
+                                )
+                        ).collect(Collectors.toList());
+        final CtMethod<?> mainMethod = initMainMethod(factory);
+        localVariables.forEach(blockMain::insertBegin);
+
+        final Iterator<CtLocalVariable<?>> iterator = localVariables.iterator();
+        blockMain.getElements(new TypeFilter<CtLiteral<?>>(CtLiteral.class))
+                .forEach(literal ->
+                    literal.replace(factory.createVariableRead(iterator.next().getReference(), false))
+                );
+        mainMethod.setBody(blockMain);
+        return mainMethod;
+    }
+
+    private static String createMakeRead(Factory factory, CtLiteral<?> literal) {
+        Object value = literal.getValue();
+        if (value instanceof String) {
+            value = "\"" + value + "\"";
+        }
+        return "catg.CATG.make" + literal.getType().getSimpleName() + "(" + value + ")";
+    }
+
+    private static CtMethod<?> initMainMethod(Factory factory) {
+        final CtMethod<Void> mainMethod = factory.createMethod();
+        mainMethod.setSimpleName("main");
+        mainMethod.addModifier(ModifierKind.PUBLIC);
+        mainMethod.addModifier(ModifierKind.STATIC);
+        mainMethod.setType(factory.Type().VOID_PRIMITIVE);
+        final CtParameter parameter = factory.createParameter();
+        parameter.setSimpleName("args");
+        parameter.setType(factory.Type().get(String[].class).getReference());
+        mainMethod.addParameter(parameter);
+        return mainMethod;
+    }
+
+}
