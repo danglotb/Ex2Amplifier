e250bd5ae4371e0f321f202be9f80eb0dfc0bdbe
danglotb <bdanglot@gmail.com>
2018-01-24 14:06:27 +0100

    grant permission to execute cvc4

--- src/bash/install-dspot.sh
+++ src/bash/install-dspot.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.4-x86_64-linux-opt
-{ echo "\ncatg.cvc4Command=" ; echo $(pwd); echo "/" ; echo $(ls | grep cvc4); } | sed ':a;N;s/\n//;ba' >> lib/catg.conf
+{ echo "catg.cvc4Command=" ; echo $(pwd); echo "/" ; echo $(ls | grep cvc4); } | sed ':a;N;s/\n//;ba' >> lib/catg.conf
+chmod +x cvc4-1.4-x86_64-linux-opt
+
+cat lib/catg.conf
 
 git clone http://github.com/STAMP-project/dspot.git
 cd dspot
