a481c9d00b45cd8d8787babf41c3b7f47251473b
danglotb <bdanglot@gmail.com>
2018-01-24 13:43:57 +0100

    ci: clone and build dspot

--- src/bash/install-dspot.sh
+++ src/bash/install-dspot.sh
@@ -5,18 +5,3 @@ cd dspot
 mvn install -DskipTests
 
 cd ..
-
-git clone http://github.com/danglotb/jbse -b pre-java8
-cd jbse
-mvn install -DskipTests
-
-cd ..
-
-mkdir lib
-cd lib
-git clone https://github.com/Z3Prover/z3.git
-cd z3
-python scripts/mk_make.py --prefix=./
-cd build
-make
-sudo make install
