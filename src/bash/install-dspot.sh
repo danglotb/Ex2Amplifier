4b46eeabeed7ec6d40b3141d3aae7fa962efa6e0
danglotb <bdanglot@gmail.com>
2018-01-24 14:04:24 +0100

    ci: update path to cvc4

--- src/bash/install-dspot.sh
+++ src/bash/install-dspot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.4-x86_64-linux-opt
+{ echo "\ncatg.cvc4Command=" ; echo $(pwd); echo "/" ; echo $(ls | grep cvc4); } | sed ':a;N;s/\n//;ba' >> lib/catg.conf
+
 git clone http://github.com/STAMP-project/dspot.git
 cd dspot
 mvn install -DskipTests
