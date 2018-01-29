2eb7418452e833eca604eacf86fa4ecb0f1dba60
Benjamin DANGLOT <benjamin.danglot@inria.fr>
2018-01-29 20:02:50 +0100

    Bool and escaping char (#13)
    * resource: add boolean and escaped char
    
    * test: refactor test to compile before compare, and update for boolean and escaped char
    
    * feat: support escaping char and boolean
    
    * feat: generate values for boolean and char
    
    * test: update test according to the resource

--- src/test/java/fr/inria/stamp/MainGeneratorTest.java
+++ src/test/java/fr/inria/stamp/MainGeneratorTest.java
@@ -42,6 +42,12 @@ public class MainGeneratorTest extends AbstractTest {
         final CtMethod<?> mainMethodFromTestMethod = MainGenerator.generateMainMethodFromTestMethod(
                 testClass.getMethodsByName("test").get(0)
         );
+
+        System.out.println(mainMethodFromTestMethod.toString());
+        testClass.addMethod(mainMethodFromTestMethod);
+        this.launcher.getModelBuilder().setBinaryOutputDirectory(new File("target/trash/"));
+        this.launcher.getModelBuilder().compile(SpoonModelBuilder.InputType.CTTYPES);
+
         final String expectedMainMethod = "public static void main(String[] args) {" + AmplificationHelper.LINE_SEPARATOR +
                 "    MainTest mainTest = new MainTest();" + AmplificationHelper.LINE_SEPARATOR +
                 "    try {" + AmplificationHelper.LINE_SEPARATOR +
@@ -52,22 +58,26 @@ public class MainGeneratorTest extends AbstractTest {
                 "    String lit1 = catg.CATG.readString(\"\\\"bar\\\"\");" + AmplificationHelper.LINE_SEPARATOR +
                 "    String lit2 = catg.CATG.readString(\"NEW\" + System.getProperty(\"line.separator\") + \"LINE\");" + AmplificationHelper.LINE_SEPARATOR +
                 "    boolean lit3 = catg.CATG.readBool(true);" + AmplificationHelper.LINE_SEPARATOR +
-                "    int lit4 = catg.CATG.readInt(100);" + AmplificationHelper.LINE_SEPARATOR +
-                "    String lit5 = catg.CATG.readString(\"Potion\");" + AmplificationHelper.LINE_SEPARATOR +
-                "    int lit6 = catg.CATG.readInt(5);" + AmplificationHelper.LINE_SEPARATOR +
-                "    String lit7 = catg.CATG.readString(\"Timoleon\");" + AmplificationHelper.LINE_SEPARATOR +
-                "    int lit8 = catg.CATG.readInt(1000);" + AmplificationHelper.LINE_SEPARATOR +
-                "    String lit9 = catg.CATG.readString(\"Potion\");" + AmplificationHelper.LINE_SEPARATOR +
+                "    char lit4 = catg.CATG.readChar('<');" + AmplificationHelper.LINE_SEPARATOR +
+                "    char lit5 = catg.CATG.readChar('\\'');" + AmplificationHelper.LINE_SEPARATOR +
+                "    int lit6 = catg.CATG.readInt(100);" + AmplificationHelper.LINE_SEPARATOR +
+                "    String lit7 = catg.CATG.readString(\"Potion\");" + AmplificationHelper.LINE_SEPARATOR +
+                "    int lit8 = catg.CATG.readInt(5);" + AmplificationHelper.LINE_SEPARATOR +
+                "    String lit9 = catg.CATG.readString(\"Timoleon\");" + AmplificationHelper.LINE_SEPARATOR +
+                "    int lit10 = catg.CATG.readInt(1000);" + AmplificationHelper.LINE_SEPARATOR +
+                "    String lit11 = catg.CATG.readString(\"Potion\");" + AmplificationHelper.LINE_SEPARATOR +
                 "    System.out.println(mainTest.aUsedNumber);" + AmplificationHelper.LINE_SEPARATOR +
                 "    System.out.println(mainTest.getANumber());" + AmplificationHelper.LINE_SEPARATOR +
                 "    System.out.println(lit1);" + AmplificationHelper.LINE_SEPARATOR +
                 "    System.out.println(lit2);" + AmplificationHelper.LINE_SEPARATOR +
                 "    System.out.println(lit3);" + AmplificationHelper.LINE_SEPARATOR +
-                "    Seller seller = new Seller(lit4, Collections.singletonList(new Item(lit5, lit6)));" + AmplificationHelper.LINE_SEPARATOR +
-                "    Player player = new Player(lit7, lit8);" + AmplificationHelper.LINE_SEPARATOR +
+                "    System.out.println(lit4);" + AmplificationHelper.LINE_SEPARATOR +
+                "    System.out.println(lit5);" + AmplificationHelper.LINE_SEPARATOR +
+                "    Seller seller = new Seller(lit6, Collections.singletonList(new Item(lit7, lit8)));" + AmplificationHelper.LINE_SEPARATOR +
+                "    Player player = new Player(lit9, lit10);" + AmplificationHelper.LINE_SEPARATOR +
                 "    player.toString();" + AmplificationHelper.LINE_SEPARATOR +
                 "    seller.toString();" + AmplificationHelper.LINE_SEPARATOR +
-                "    player.buyItem(lit9, seller);" + AmplificationHelper.LINE_SEPARATOR +
+                "    player.buyItem(lit11, seller);" + AmplificationHelper.LINE_SEPARATOR +
                 "    player.toString();" + AmplificationHelper.LINE_SEPARATOR +
                 "    seller.toString();" + AmplificationHelper.LINE_SEPARATOR +
                 "    try {" + AmplificationHelper.LINE_SEPARATOR +
@@ -77,11 +87,5 @@ public class MainGeneratorTest extends AbstractTest {
                 "    }" + AmplificationHelper.LINE_SEPARATOR +
                 "}";
         assertEquals(expectedMainMethod, mainMethodFromTestMethod.toString());
-
-        System.out.println(mainMethodFromTestMethod.toString());
-
-        testClass.addMethod(mainMethodFromTestMethod);
-        this.launcher.getModelBuilder().setBinaryOutputDirectory(new File("target/trash/"));
-        this.launcher.getModelBuilder().compile(SpoonModelBuilder.InputType.CTTYPES);
     }
 }
