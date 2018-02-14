244c05c27201a5dee0605f55491a0fce27407578
danglotb <bdanglot@gmail.com>
2018-02-14 14:08:09 +0100

    test: update the expected method as string

--- src/test/java/fr/inria/stamp/catg/MainGeneratorTest.java
+++ src/test/java/fr/inria/stamp/catg/MainGeneratorTest.java
@@ -46,43 +46,47 @@ public class MainGeneratorTest extends AbstractTest {
         this.launcher.getModelBuilder().setBinaryOutputDirectory(new File("target/trash/"));
         this.launcher.getModelBuilder().compile(SpoonModelBuilder.InputType.CTTYPES);
 
-        final String expectedMainMethod = "public static void main(String[] args) {" + AmplificationHelper.LINE_SEPARATOR +
-                "    MainTest mainTest = new MainTest();" + AmplificationHelper.LINE_SEPARATOR +
-                "    try {" + AmplificationHelper.LINE_SEPARATOR +
-                "        mainTest.setUp();" + AmplificationHelper.LINE_SEPARATOR +
-                "    } catch (Exception __exceptionEx2Amplifier) {" + AmplificationHelper.LINE_SEPARATOR +
-                "        throw new RuntimeException(__exceptionEx2Amplifier);" + AmplificationHelper.LINE_SEPARATOR +
-                "    }" + AmplificationHelper.LINE_SEPARATOR +
-                "    String lit1 = catg.CATG.readString(\"\\\"bar\\\"\");" + AmplificationHelper.LINE_SEPARATOR +
-                "    String lit2 = catg.CATG.readString(\"NEW\" + System.getProperty(\"line.separator\") + \"LINE\");" + AmplificationHelper.LINE_SEPARATOR +
-                "    boolean lit3 = catg.CATG.readBool(true);" + AmplificationHelper.LINE_SEPARATOR +
-                "    char lit4 = catg.CATG.readChar('<');" + AmplificationHelper.LINE_SEPARATOR +
-                "    char lit5 = catg.CATG.readChar('\\'');" + AmplificationHelper.LINE_SEPARATOR +
-                "    int lit6 = catg.CATG.readInt(100);" + AmplificationHelper.LINE_SEPARATOR +
-                "    String lit7 = catg.CATG.readString(\"Potion\");" + AmplificationHelper.LINE_SEPARATOR +
-                "    int lit8 = catg.CATG.readInt(5);" + AmplificationHelper.LINE_SEPARATOR +
-                "    String lit9 = catg.CATG.readString(\"Timoleon\");" + AmplificationHelper.LINE_SEPARATOR +
-                "    int lit10 = catg.CATG.readInt(1000);" + AmplificationHelper.LINE_SEPARATOR +
-                "    String lit11 = catg.CATG.readString(\"Potion\");" + AmplificationHelper.LINE_SEPARATOR +
-                "    System.out.println(mainTest.aUsedNumber);" + AmplificationHelper.LINE_SEPARATOR +
-                "    System.out.println(mainTest.getANumber());" + AmplificationHelper.LINE_SEPARATOR +
-                "    System.out.println(lit1);" + AmplificationHelper.LINE_SEPARATOR +
-                "    System.out.println(lit2);" + AmplificationHelper.LINE_SEPARATOR +
-                "    System.out.println(lit3);" + AmplificationHelper.LINE_SEPARATOR +
-                "    System.out.println(lit4);" + AmplificationHelper.LINE_SEPARATOR +
-                "    System.out.println(lit5);" + AmplificationHelper.LINE_SEPARATOR +
-                "    Seller seller = new Seller(lit6, Collections.singletonList(new Item(lit7, lit8)));" + AmplificationHelper.LINE_SEPARATOR +
-                "    Player player = new Player(lit9, lit10);" + AmplificationHelper.LINE_SEPARATOR +
-                "    player.toString();" + AmplificationHelper.LINE_SEPARATOR +
-                "    seller.toString();" + AmplificationHelper.LINE_SEPARATOR +
-                "    player.buyItem(lit11, seller);" + AmplificationHelper.LINE_SEPARATOR +
-                "    player.toString();" + AmplificationHelper.LINE_SEPARATOR +
-                "    seller.toString();" + AmplificationHelper.LINE_SEPARATOR +
-                "    try {" + AmplificationHelper.LINE_SEPARATOR +
-                "        mainTest.tearDown();" + AmplificationHelper.LINE_SEPARATOR +
-                "    } catch (Exception __exceptionEx2Amplifier) {" + AmplificationHelper.LINE_SEPARATOR +
-                "        throw new RuntimeException(__exceptionEx2Amplifier);" + AmplificationHelper.LINE_SEPARATOR +
-                "    }" + AmplificationHelper.LINE_SEPARATOR +
+        final String expectedMainMethod = "public static void main(String[] args) {" + AmplificationHelper.LINE_SEPARATOR + 
+                "    MainTest mainTest = new MainTest();" + AmplificationHelper.LINE_SEPARATOR + 
+                "    try {" + AmplificationHelper.LINE_SEPARATOR + 
+                "        mainTest.setUp();" + AmplificationHelper.LINE_SEPARATOR + 
+                "    } catch (Exception __exceptionEx2Amplifier) {" + AmplificationHelper.LINE_SEPARATOR + 
+                "        throw new RuntimeException(__exceptionEx2Amplifier);" + AmplificationHelper.LINE_SEPARATOR + 
+                "    }" + AmplificationHelper.LINE_SEPARATOR + 
+                "    try {" + AmplificationHelper.LINE_SEPARATOR + 
+                "        String lit1 = catg.CATG.readString(\"\\\"bar\\\"\");" + AmplificationHelper.LINE_SEPARATOR + 
+                "        String lit2 = catg.CATG.readString(\"NEW\" + System.getProperty(\"line.separator\") + \"LINE\");" + AmplificationHelper.LINE_SEPARATOR + 
+                "        boolean lit3 = catg.CATG.readBool(true);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        char lit4 = catg.CATG.readChar('<');" + AmplificationHelper.LINE_SEPARATOR + 
+                "        char lit5 = catg.CATG.readChar('\\'');" + AmplificationHelper.LINE_SEPARATOR + 
+                "        int lit6 = catg.CATG.readInt(100);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        String lit7 = catg.CATG.readString(\"Potion\");" + AmplificationHelper.LINE_SEPARATOR + 
+                "        int lit8 = catg.CATG.readInt(5);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        String lit9 = catg.CATG.readString(\"Timoleon\");" + AmplificationHelper.LINE_SEPARATOR + 
+                "        int lit10 = catg.CATG.readInt(1000);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        String lit11 = catg.CATG.readString(\"Potion\");" + AmplificationHelper.LINE_SEPARATOR + 
+                "        System.out.println(mainTest.aUsedNumber);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        System.out.println(mainTest.getANumber());" + AmplificationHelper.LINE_SEPARATOR + 
+                "        System.out.println(lit1);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        System.out.println(lit2);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        System.out.println(lit3);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        System.out.println(lit4);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        System.out.println(lit5);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        Seller seller = new Seller(lit6, Collections.singletonList(new Item(lit7, lit8)));" + AmplificationHelper.LINE_SEPARATOR + 
+                "        Player player = new Player(lit9, lit10);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        player.toString();" + AmplificationHelper.LINE_SEPARATOR + 
+                "        seller.toString();" + AmplificationHelper.LINE_SEPARATOR + 
+                "        player.buyItem(lit11, seller);" + AmplificationHelper.LINE_SEPARATOR + 
+                "        player.toString();" + AmplificationHelper.LINE_SEPARATOR + 
+                "        seller.toString();" + AmplificationHelper.LINE_SEPARATOR + 
+                "    } catch (Exception __exceptionEx2AmplifierException) {" + AmplificationHelper.LINE_SEPARATOR + 
+                "        throw new RuntimeException(__exceptionEx2AmplifierException);" + AmplificationHelper.LINE_SEPARATOR + 
+                "    }" + AmplificationHelper.LINE_SEPARATOR + 
+                "    try {" + AmplificationHelper.LINE_SEPARATOR + 
+                "        mainTest.tearDown();" + AmplificationHelper.LINE_SEPARATOR + 
+                "    } catch (Exception __exceptionEx2Amplifier) {" + AmplificationHelper.LINE_SEPARATOR + 
+                "        throw new RuntimeException(__exceptionEx2Amplifier);" + AmplificationHelper.LINE_SEPARATOR + 
+                "    }" + AmplificationHelper.LINE_SEPARATOR + 
                 "}";
         assertEquals(expectedMainMethod, mainMethodFromTestMethod.toString());
     }
