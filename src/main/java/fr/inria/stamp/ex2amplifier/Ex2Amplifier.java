813ff6e48a1fa27521caeb9eae0c02c8a75ef03b
Benjamin DANGLOT <benjamin.danglot@inria.fr>
2018-02-27 21:34:44 +0100

    feat: add intermediate output of amplified tests (#26)

--- src/main/java/fr/inria/stamp/ex2amplifier/Ex2Amplifier.java
+++ src/main/java/fr/inria/stamp/ex2amplifier/Ex2Amplifier.java
@@ -1,14 +1,18 @@
 package fr.inria.stamp.ex2amplifier;
 
 import fr.inria.diversify.dspot.amplifier.Amplifier;
+import fr.inria.diversify.utils.AmplificationHelper;
 import fr.inria.diversify.utils.sosiefier.InputConfiguration;
 import spoon.reflect.code.CtLiteral;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.visitor.filter.TypeFilter;
 
+import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Benjamin DANGLOT
@@ -31,11 +35,14 @@ public class Ex2Amplifier implements Amplifier {
         }
     };
 
+    private InputConfiguration configuration;
+
     public void init(InputConfiguration configuration) {
         this.init(configuration, Ex2Amplifier_Mode.CATG);
     }
 
     public void init(InputConfiguration configuration, Ex2Amplifier_Mode mode) {
+        this.configuration = configuration;
         if (mode == Ex2Amplifier_Mode.CATG) {
             this.amplifier = new CATGAmplifier(configuration);
         } else {
@@ -48,7 +55,24 @@ public class Ex2Amplifier implements Amplifier {
         if (ctMethod.getElements(CT_LITERAL_TYPE_FILTER).isEmpty()) {
             return Collections.emptyList();
         }
-        return this.amplifier.apply(ctMethod);
+        final List<CtMethod> amplifiedTests = this.amplifier.apply(ctMethod);
+        if (!amplifiedTests.isEmpty()) {
+            this.printAmplifiedTest(amplifiedTests);
+        }
+        return amplifiedTests;
+    }
+
+    // this output is meant to be used in the manual analysis of the result
+    // in order to understand how it succeed or why the Ex2Amplifier is failing to catch the change behavior
+    private void printAmplifiedTest(List<CtMethod> amplifiedTests) {
+        try (FileWriter writer = new FileWriter(this.configuration.getOutputDirectory() + "amplified_tests.txt", true)) {
+            writer.write(amplifiedTests.stream()
+                    .map(CtMethod::toString)
+                    .collect(Collectors.joining(AmplificationHelper.LINE_SEPARATOR))
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
